167e7621414c9d241faaf00482c3cda32dfc7475
Wellington Kato <wellingtonkato@gmail.com>
2019-05-20 23:03:07 -0300

    Projeto criado

---
+++ src/main/java/com/kato/cursomcv2/Cursomcv2Application.java
@@ -0,0 +1,13 @@
+package com.kato.cursomcv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cursomcv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cursomcv2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/kato/cursomcv2/Cursomcv2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kato.cursomcv2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Cursomcv2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
