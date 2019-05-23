98cf79e88e2fda9423d504862fd878b61dbb1e91
Wellington Kato <wellingtonkato@gmail.com>
2019-05-22 23:24:54 -0300

    Produto e associação muitos pra muitos

--- src/main/java/com/kato/cursomcv2/Cursomcv2Application.java
+++ src/main/java/com/kato/cursomcv2/Cursomcv2Application.java
@@ -8,7 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.kato.cursomcv2.domain.Categoria;
+import com.kato.cursomcv2.domain.Produto;
 import com.kato.cursomcv2.repositories.CategoriaRepository;
+import com.kato.cursomcv2.repositories.ProdutoRepository;
 
 //CommandLineRunner é usado para instaciar algo no inicio
 
@@ -18,6 +20,8 @@ public class Cursomcv2Application implements CommandLineRunner{
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Cursomcv2Application.class, args);
@@ -29,9 +33,20 @@ public class Cursomcv2Application implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		Produto 	p1 = new Produto(null, "Computador", 2000.00);
+		Produto		p2 = new Produto(null, "Impressora", 800.00);
+		Produto		p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		
-		
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 
 }
